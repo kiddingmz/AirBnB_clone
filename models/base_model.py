6821081c88f9709d794931671dd0b50f4a770975
feliciodeangela <feliciodeangela@gmail.com>
2023-10-10 06:48:49 -0700

    FIX: Correct instance attributes initialization

--- models/base_model.py
+++ models/base_model.py
@@ -9,9 +9,9 @@ class BaseModel:
 
     def __init__(self):
         """"BaseModel constructor"""
-        id = str(uuid.uuid4())
-        created_at = datetime.now().isoformat()
-        updated_at = datetime.now().isoformat()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
         """Prints the string format representation of a BaseModel object"""
