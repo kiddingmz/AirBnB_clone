43e999e03e0bae42072146fa759e8749c16c38c8
felicio <felicio@localhost.localdomain>
2023-10-10 23:42:43 +0200

    CHORE: implementation of the variant attributes adition to the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,15 @@ import uuid
 class BaseModel:
     """Defines common attributes/methods for hbnb classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """"BaseModel constructor"""
+        for key, val in kwargs.items():
+            if key == '__class__':
+                continue
+            if key == 'created_at' or key == 'updated_at':
+                setattr(self, key, datetime.fromisoformat(val))
+                continue
+            setattr(self, key, val)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -27,7 +34,7 @@ class BaseModel:
 
         Returns:
             A dictionary containing all keys/values"""
-        #new_dict = {'__class__': type(self).__name__}
+        # new_dict = {'__class__': type(self).__name__}
         new_dict = {**self.__dict__}
         new_dict['__class__'] = type(self).__name__
         new_dict['updated_at'] = self.updated_at.isoformat()
