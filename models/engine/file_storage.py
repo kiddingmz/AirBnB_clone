ef2cc976c2355ff05ef97e7c6167fa2c98585b49
kiddingmz <bata.isidro@gmail.com>
2023-10-11 18:34:02 +0200

    CHORE: file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Defines the file storage"""
 import os
-
+from json import load, dump, dumps
+from models.base_model import BaseModel
 
 class FileStorage():
     """Class File Storage"""
@@ -19,12 +20,20 @@ class FileStorage():
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
+        output = dict()
+        for k, v in self.__objects.items():
+            output[k] = v.to_dict()
         self.__file_path = "storage.json"
+        print(output)
         with open(self.__file_path, "a+") as file:
-            file.write(self.__objects)
+            dump(output, file)
+            file.write(str('\n'))
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file"""
+        self.__file_path = "storage.json"
         if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r") as file:
-                file.read(loads(self.__objects))
+            with open(self.__file_path) as file:
+                #load(file)
+                print("1")
+
