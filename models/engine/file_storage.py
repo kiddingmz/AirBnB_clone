ecc00d83bd62eb07e32b34aa6d4f593cad13b1a0
kiddingmz <bata.isidro@gmail.com>
2023-10-12 23:10:03 +0200

    CHORE: improvements  method reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,9 @@
 import os
 from json import load, dump, dumps
 from models.base_model import BaseModel
+from datetime import datetime
+
+
 
 class FileStorage():
     """Class File Storage"""
@@ -20,24 +23,29 @@ class FileStorage():
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        #output = dict()
+        output = {k: v.to_dict() for k, v in self.__objects.items()}
         #for k, v in self.__objects.items():
-        #    output[k] = v.to_dict()
-        #self.__file_path = "storage.json"
-        #with open(self.__file_path, "a+") as file:
-        #    dump(output, file)
-        #    file.write(str('\n'))
-        #out = dict()
-        #check = {k: v.to_dict() for k, v in self.__objects.items()}
-        #print("Start")
-        #print(check)
-        #print("End")
-        #with open(self.__file_path, "a") as file:
-        #    dump(check, file)
+            #output[k] = v.to_dict()
+        self.__file_path = "storage.json"
+        with open(self.__file_path, "w") as file:
+            dump(output, file)
+            file.write(str('\n'))
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file"""
         self.__file_path = "storage.json"
-        #if os.path.exists(self.__file_path):
-            #with open(self.__file_path) as file:
-                #self.__objects = load(file)
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path) as file:
+                #print(load(file))
+                for k, v in load(file).items():
+                    #print(k, v)
+                    base_name = k.split('.')[0]
+                    id = k.split('.')[1]
+                    v['created_at'] = datetime.fromisoformat(v['created_at'])
+                    v['updated_at'] = datetime.fromisoformat(v['updated_at'])
+                    del(v['__class__'])
+                    new_dict = {base_name:v}
+                print(new_dict)
+                    #string = "[{}] ({}) {}".format(base_name, id, v)
+                #print(**string.__dict__)
+                
