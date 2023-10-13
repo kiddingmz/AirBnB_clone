372cd1051cb43c67f057372410a63cbcd08a2f1b
feliciodeangela <feliciodeangela@gmail.com>
2023-10-13 03:56:15 -0700

    FEAT: Implementing the console's base structure

--- console.py
+++ console.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """Defines the Console"""
+import cmd
 
 
-
-def console():
+class HBNBCommand(cmd.Cmd):
     """Console"""
-    pass
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        pass
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """Quit command to exit the program
+        """
+        self.do_quit
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
