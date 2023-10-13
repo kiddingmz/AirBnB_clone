0b26230679441688007b9711aa6bf775fbb33fcc
kiddingmz <bata.isidro@gmail.com>
2023-10-13 23:07:30 +0200

    FEAT: Add methods create and show

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines the Console"""
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,6 +21,32 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program
         """
         self.do_quit
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel"""
+        try:
+            obj = eval(line.split(' ')[0])()
+        except NameError:
+            print("** class doesn't exist **")
+        except SyntaxError:
+            print("** class name missing **")
+        else:
+            print(obj)
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an instance based on the\
+                class name and id"""
+        db = storage.all()
+        cls = db.items()[0]
+        print(cls)
+       # obj = eval(line.split(' ')[0])()
+       # print(dir(obj))
+        #cls = line.split(' ')[0]
+        #new_dict = obj.all()
+        #id = line.split(' ')[1]
+        #print(new_dict, cls, id)
 
 
 if __name__ == '__main__':
