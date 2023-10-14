95a46dcf036c56fa1978c26f684b5e27f7969a0a
felicio <feliciodeangela@gmail.com>
2023-10-14 23:53:55 +0200

    FEAT: Add the all method

--- console.py
+++ console.py
@@ -33,7 +33,6 @@ class HBNBCommand(cmd.Cmd):
         except SyntaxError:
             print("** class name missing **")
         else:
-            print(obj)
             obj.save()
             print(obj.id)
 
@@ -45,17 +44,49 @@ instance based on the class name and id
         kys = db.keys()
         cls_line = line.split(' ')[0]
         id_line = line.split(' ')[1]
-        print(db)
-        for ks in kys:
-            if cls_line == ks.split('.')[0] and id_line == ks.split('.')[1]:
-                print(db[ks])
-                break
+        if "{}.{}".format(cls_line, id_line) in db:
+            print(db["{}.{}".format(cls_line, id_line)])
         # obj = eval(line.split(' ')[0])()
         # print(dir(obj))
         # cls = line.split(' ')[0]
         # new_dict = obj.all()
         # id = line.split(' ')[1]
         # print(new_dict, cls, id)
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id
+        """
+        db = storage.all()
+        kys = db.keys()
+        cls_line = line.split(' ')[0]
+        id_line = line.split(' ')[1]
+        if "{}.{}".format(cls_line, id_line) in db:
+            del (db["{}.{}".format(cls_line, id_line)])
+        storage.save()
+
+    def do_all(self, line):
+        """Prints all string representation of all instances \
+based or not on the class name
+        """
+        db = storage.all()
+        kys = db.keys()
+        cls_line = line.split(' ')[0]
+        bm_list = []
+        for ks in kys:
+            if cls_line == ks.split('.')[0]:
+                bm_list.append(str(db[ks]))
+        print(bm_list)
+
+    def do_update(self, line):
+        """Updates an instance based on the class name and id \
+by adding or updating attribute
+        """
+        db = storage.all()
+        kys = db.keys()
+        cls_line = line.split(' ')[0]
+        id_line = line.split(' ')[1]
+        if "{}.{}".format(cls_line, id_line) in db:
+            pass
 
 
 if __name__ == '__main__':
