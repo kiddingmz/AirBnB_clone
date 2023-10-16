dd1993d52ab21c3098ca2df01576ce3058cb984c
kiddingmz <bata.isidro@gmail.com>
2023-10-16 03:33:44 +0200

    CHORE: console

--- console.py
+++ console.py
@@ -14,6 +14,52 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
     """Console"""
     prompt = "(hbnb) "
+    valid_classes = ['BaseModel', 'User', 'Amenity', 'Review', 'State', 'City',
+                     'Place']
+    ERROR_CLASS_NAME = '** class name missing **'
+    ERROR_CLASS = "** class doesn't exist **"
+    ERROR_ID = "** instance id missing **"
+    ERROR_ID_NOT_FOUND = "** no instance found **"
+    ERROR_ATTR = "** attribute name missing **"
+    ERROR_ATTR_VALUE = "** value missing **"
+
+    def validate_len_args(self, line):
+        if len(line) == 0:
+            print(self.ERROR_CLASS_NAME)
+            return False
+        return True
+
+    def validate_class_name(self, line):
+        args = line.split(' ')
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print(self.ERROR_CLASS)
+            return False
+        return class_name
+
+    def validate_id(self, line):
+        args = line.split(' ')
+        if len(args) < 2:
+            print(self.ERROR_ID)
+            return False
+        id_number = args[1]
+        return id_number
+
+    def validate_attr(self, line):
+        args = line.split(' ')
+        if len(args) < 3:
+            print(self.ERROR_ATTR)
+            return False
+        attribute = args[2]
+        return attribute
+
+    def validate_attr_value(self, line):
+        args = line.split(' ')
+        if len(args) < 4:
+            print(self.ERROR_ATTR_VALUE)
+            return False
+        attr_value = args[3]
+        return attr_value
 
     def emptyline(self):
         """Empty input"""
@@ -27,12 +73,13 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """Quit command to exit the program
         """
+        print()
         self.do_quit
 
     def do_create(self, line):
         """Creates a new instance of BaseModel
         """
-        try:
+        '''try:
             obj = eval(line.split(' ')[0])()
         except NameError:
             print("** class doesn't exist **")
@@ -41,17 +88,38 @@ class HBNBCommand(cmd.Cmd):
         else:
             obj.save()
             print(obj.id)
+        '''
+        if not self.validate_len_args(line):
+            return
+
+        cls_name = self.validate_class_name(line)
+        if not cls_name:
+            return
+
+        obj = eval(cls_name)()
+        obj.save()
+        print(obj.id)
 
     def do_show(self, line):
         """Prints the string representation of an \
 instance based on the class name and id
         """
+        if not self.validate_len_args(line):
+            return
+
+        cls_name = self.validate_class_name(line)
+        if not cls_name:
+            return
+
+        id_line = self.validate_id(line)
+        if not id_line:
+            return
+
         db = storage.all()
-        kys = db.keys()
-        cls_line = line.split(' ')[0]
-        id_line = line.split(' ')[1]
-        if "{}.{}".format(cls_line, id_line) in db:
-            print(db["{}.{}".format(cls_line, id_line)])
+        if "{}.{}".format(cls_name, id_line) in db:
+            print(db["{}.{}".format(cls_name, id_line)])
+        else:
+            print(self.ERROR_ID_NOT_FOUND)
         # obj = eval(line.split(' ')[0])()
         # print(dir(obj))
         # cls = line.split(' ')[0]
@@ -62,13 +130,23 @@ instance based on the class name and id
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id
         """
+        if not self.validate_len_args(line):
+            return
+
+        cls_name = self.validate_class_name(line)
+        if not cls_name:
+            return
+
+        id_line = self.validate_id(line)
+        if not id_line:
+            return
+
         db = storage.all()
-        kys = db.keys()
-        cls_line = line.split(' ')[0]
-        id_line = line.split(' ')[1]
-        if "{}.{}".format(cls_line, id_line) in db:
-            del (db["{}.{}".format(cls_line, id_line)])
-        storage.save()
+        if "{}.{}".format(cls_name, id_line) in db:
+            del (db["{}.{}".format(cls_name, id_line)])
+            storage.save()
+        else:
+            print(self.ERROR_ID_NOT_FOUND)
 
     def do_all(self, line):
         """Prints all string representation of all instances \
@@ -76,27 +154,52 @@ based or not on the class name
         """
         db = storage.all()
         kys = db.keys()
-        cls_line = line.split(' ')[0]
-        bm_list = []
+        cls_line = self.validate_class_name(line)
+        bm_list = list()
         for ks in kys:
             if cls_line == ks.split('.')[0]:
                 bm_list.append(str(db[ks]))
-        print(bm_list)
+        if len(bm_list) > 0:
+            print(bm_list)
 
     def do_update(self, line):
         """Updates an instance based on the class name and id \
 by adding or updating attribute
         """
+        if not self.validate_len_args(line):
+            return
+
+        cls_line = self.validate_class_name(line)
+        if not cls_line:
+            return
+
+        id_line = self.validate_id(line)
+        if not id_line:
+            return
+
+        key_line = self.validate_attr(line)
+        if not key_line:
+            return
+
+        val_line = self.validate_attr_value(line)
+        if not val_line:
+            return
+
         db = storage.all()
         kys = db.keys()
-        cls_line = line.split(' ')[0]
-        id_line = line.split(' ')[1]
-        key_line = line.split(' ')[2]
-        val_line = line.split('"')[1]
         if "{}.{}".format(cls_line, id_line) in db:
+            if val_line.isdigit():
+                val_line = int(val_line)
+            else:
+                try:
+                    val_line = float(val_line)
+                except:
+                    pass
+
             setattr(db["{}.{}".format(cls_line, id_line)], key_line, val_line)
             storage.save()
-
+        else:
+            print(self.ERROR_ID_NOT_FOUND)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
